202d4a0c19bf584cdeedcd3b6897ed2cd05e9f49
Brayan Oviedo <brayan.oviedo@ceiba.com.co>
2025-06-22 18:00:05 -0500

    Template base

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
 
-  //basePath: process.env.NODE_ENV === 'production' ? '/' : '',
-  //assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/web-ugc' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/web-ugc/' : '',
 
   images: {
     unoptimized: true,
