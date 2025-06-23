9dd519969acc16b697c89a0377ea29bc6bb73f93
Brayan Oviedo <brayan.oviedo@ceiba.com.co>
2025-06-23 17:11:30 -0500

    Template base

--- src/config/AOSProvider.tsx
+++ src/config/AOSProvider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function AOSProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
