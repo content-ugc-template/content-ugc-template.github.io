5998a327b89c6b89175baa6d12add520ccbdf212
Brayan Oviedo <brayan.oviedo@ceiba.com.co>
2025-06-22 19:46:43 -0500

    Template base

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="flex items-center h-12 w-full bg-black p-4">
+    <div className="flex items-center h-12 w-full bg-black p-4 font-bold text-md">
       <span className="text-white">UGC</span>
     </div>
   );
